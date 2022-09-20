89d041c2e785246b4cb74209591e4550c50ca839
BrandelleroEzequiel <ezequielbrandellero@gmail.com>
2022-09-19 23:21:23 -0300

    Controlador3 Rama3

---
+++ Ejemplo GIT/Controllers/Controlador3.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_GIT.Controllers
+{
+    public class Controlador3 : Controller
+    {
+        [HttpGet]
+        [Route("Controllador3")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
