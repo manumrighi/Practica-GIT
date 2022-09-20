8005fe2058b5e289b413501fe97fdc828fdaf0df
manumrighi <manuelmorichi08@gmail.com>
2022-09-20 19:22:01 -0300

    controlador 4 de manuel

---
+++ Ejemplo GIT/Controllers/Controlador4.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_GIT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controlador4 : ControllerBase
+    {
+        //controladorDeManuel
+    }
+}
