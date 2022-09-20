8d9953d3de3fcbdda023573db3ef157c45f27e2a
Dino <gamerdino41@gmail.com>
2022-09-20 19:24:15 -0300

    Cotrolador4 rama4

---
+++ Ejemplo GIT/Controllers/Cotrolador4.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_GIT.Controllers
+{
+    public class Controlador4 : Controller
+    {
+        public IActionResult Index()
+        {
+           //Dino
+        }
+    }
+}
